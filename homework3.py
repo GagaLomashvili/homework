bf07e9a6f239505c8fd92f78af3d70ab687d7777
GagaLomashvili <138609160+GagaLomashvili@users.noreply.github.com>
2023-07-11 10:30:03 -0600

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+name = "Gabriel"
+surname = "Lomashvili"
+age = 13
+height = 1.75
+country = "Canada"
+city = "Toronto"
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + country + " " + city)
